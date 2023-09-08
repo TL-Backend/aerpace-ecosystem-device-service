21efc5a2e351bf11ce829ef13774937699ec6c11
Bharath Kandula <62982842+bharathkandula99@users.noreply.github.com>
2023-09-08 13:49:17 +0530

    Resolved merge conflicts

--- src/controllers/device/device.constant.js
+++ src/controllers/device/device.constant.js
@@ -1,20 +1,16 @@
-exports.errorMessages = {
-  INVALID_DEVICE_TYPE: (value) => {
-    return `Invalid device type ${value}`;
-  },
-};
-exports.successMessages = {
-  DEVICES_FETCHED_MESSAGE: 'devices fetched successfully',
-};
 
 exports.successResponses = {
   COUNT_FETCH_SUCCESSFULL: 'data fetched successful',
   NO_DATA_FOUND: 'no data found',
   DATA_FETCH_SUCCESSFULL: 'data fetched successfully',
+  DEVICES_FETCHED_MESSAGE: 'devices fetched successfully',
 };
 
 exports.errorResponses = {
   INTERNAL_ERROR: 'internal error',
+  INVALID_DEVICE_TYPE: (value) => {
+    return `Invalid device type ${value}`;
+  }
 };
 
 exports.status = {
