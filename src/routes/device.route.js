81ce13986157fc6e7c14ee0fdf32911b59ee9d5c
Bharath Kandula <62982842+bharathkandula99@users.noreply.github.com>
2023-09-08 14:56:09 +0530

    Re-aligned the routes order

--- src/routes/device.route.js
+++ src/routes/device.route.js
@@ -8,6 +8,6 @@ const {
 } = require('../controllers/device/device.middleware');
 
 module.exports = function (app) {
-  app.get('/devices/:device_type', validateGetDevicesTypeInput, getDevicesList);
   app.get('/devices/types', listDevicesTypes);
+  app.get('/devices/:device_type', validateGetDevicesTypeInput, getDevicesList);
 };
