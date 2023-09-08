81c72ead521c67df73b7d277082918d6498a8ca9
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-09-08 15:49:02 +0530

    changed query to handle empty version,variant

--- src/controllers/device/device.middleware.js
+++ src/controllers/device/device.middleware.js
@@ -1,23 +1,23 @@
-const {errorResponses} = require('./device.constant');
-const {logger} = require('../../utils/logger');
-const {errorResponse} = require('../../utils/responseHandler');
-const {statusCodes} = require('../../utils/statusCode');
-const {errorMessages} = require('./device.constant');
+const { errorResponses } = require('./device.constant');
+const { logger } = require('../../utils/logger');
+const { errorResponse } = require('../../utils/responseHandler');
+const { statusCodes } = require('../../utils/statusCode');
+const { errorMessages } = require('./device.constant');
 exports.validateGetDevicesTypeInput = async (request, response, next) => {
-    try {
-        const deviceType = request.params.device_type; // Device type values are like car, drone etc..
-        if (!deviceType || typeof deviceType !== 'string' || !deviceType?.trim()) {
-            throw errorResponses.INVALID_DEVICE_TYPE(deviceType);
-        }
-        return next();
-    } catch (error) {
-        logger.error(error);
-        return errorResponse({
-            request,
-            response,
-            error,
-            message: error,
-            code: statusCodes.STATUS_CODE_INVALID_FORMAT,
-        });
+  try {
+    const deviceType = request.params.device_type; // Device type values are like car, drone etc..
+    if (!deviceType || typeof deviceType !== 'string' || !deviceType?.trim()) {
+      throw errorResponses.INVALID_DEVICE_TYPE(deviceType);
     }
-}
+    return next();
+  } catch (error) {
+    logger.error(error);
+    return errorResponse({
+      request,
+      response,
+      error,
+      message: error,
+      code: statusCodes.STATUS_CODE_INVALID_FORMAT,
+    });
+  }
+};
