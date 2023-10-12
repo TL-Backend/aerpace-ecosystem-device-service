4a0481804e04979f60ca727c3348a135c71e12c1
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-10-12 16:15:51 +0530

    made changes in validations

--- src/controllers/device/device.middleware.js
+++ src/controllers/device/device.middleware.js
@@ -3,6 +3,7 @@ const { logger } = require('../../utils/logger');
 const { errorResponse } = require('../../utils/responseHandler');
 const { statusCodes } = require('../../utils/statusCode');
 const { errorMessages } = require('./device.constant');
+const { levelStarting } = require('../../utils/constant');
 exports.validateGetDevicesTypeInput = async (request, response, next) => {
   try {
     const deviceType = request.params.device_type; // Device type values are like car, drone etc..
@@ -26,13 +27,25 @@ exports.validateGetPersonalityPrivilegesInput = async (req, res, next) => {
   try {
     const { model_id, variant_id, version_id } = req.query;
     let errorList = [];
-    if (!model_id || typeof model_id !== 'string') {
+    if (
+      !model_id ||
+      typeof model_id !== 'string' ||
+      !model_id.startsWith(levelStarting.model)
+    ) {
       errorList.push(errorResponses.MODEL_ID_INVALID);
     }
-    if (!variant_id || typeof variant_id !== 'string') {
+    if (
+      !variant_id ||
+      typeof variant_id !== 'string' ||
+      !variant_id.startsWith(levelStarting.variant)
+    ) {
       errorList.push(errorResponses.VARIANT_ID_INVALID);
     }
-    if (!version_id || typeof version_id !== 'string') {
+    if (
+      !version_id ||
+      typeof version_id !== 'string' ||
+      !version_id.startsWith(levelStarting.version)
+    ) {
       errorList.push(errorResponses.VERSION_ID_INVALID);
     }
     if (errorList.length) {
