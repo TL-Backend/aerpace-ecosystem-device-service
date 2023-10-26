2a06f87ec2e063fca1cacc72ee352c70ee02f25e
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-10-26 10:30:50 +0530

    resolved sonar duplicate issues

--- src/controllers/device/device.helper.js
+++ src/controllers/device/device.helper.js
@@ -448,7 +448,7 @@ exports.getDeviceTypes = async () => {
     const counts = await sequelize.query(queries.getDeviceCount);
     return {
       success: true,
-      message: successResponses.DATA_FETCH_SUCCESSFULL,
+      message: successResponses.COUNT_FETCH_SUCCESSFULL,
       data: counts[0],
     };
   } catch (err) {
@@ -563,117 +563,6 @@ exports.editDevicesHelper = async (params) => {
       success: true,
       message: successResponses.DEVICES_EDITED_SUCCESSFULLY,
       data: null,
-    };
-  } catch (err) {
-    await transaction.rollback();
-    logger.error(err);
-    return {
-      success: false,
-      message: err.message,
-      errorCode: errorResponses.INTERNAL_ERROR,
-      data: null,
-    };
-  }
-};
-
-exports.addDeviceActions = async ({
-  modelId,
-  variantId,
-  versionId,
-  privileges,
-  type,
-}) => {
-  const transaction = await sequelize.transaction();
-  try {
-    let isError = [];
-    let validActions = ``;
-    await eachLimitPromise(privileges, 10, async (privilege) => {
-      const categories = await sequelize.query(getCategoriesQuery, {
-        replacements: { id: privilege.category_id },
-      });
-
-      if (!categories[0].length) {
-        isError.push('false');
-        return;
-      }
-
-      const actionsCheck = await sequelize.query(verifyActionsById, {
-        replacements: { actions: privilege.actions },
-      });
-
-      if (!actionsCheck[0][0].result) {
-        isError.push('false');
-        return;
-      }
-
-      if (versionId && variantId && modelId) {
-        const validActionsQuery = getValidActionsForVersion;
-        let aValidActions = await sequelize.query(validActionsQuery, {
-          replacements: {
-            actions: privilege.actions,
-            model_id: modelId,
-            variant_id: variantId,
-            version_id: versionId,
-          },
-          raw: true,
-        });
-        validActions = aValidActions[0][0].array;
-      }
-      if (variantId && modelId && !versionId) {
-        const validActionsQuery = getValidActionsForVariant;
-        let aValidActions = await sequelize.query(validActionsQuery, {
-          replacements: {
-            actions: privilege.actions,
-            model_id: modelId,
-            variant_id: variantId,
-            version_id: versionId,
-          },
-          raw: true,
-        });
-        validActions = aValidActions[0][0].array;
-      }
-      if (modelId && !variantId && !versionId) {
-        validActions = privilege.actions;
-      }
-
-      if (validActions.length) {
-        for (const action of validActions) {
-          await aergov_device_actions.create(
-            {
-              ...(modelId && { model_id: modelId }),
-              ...(variantId && { variant_id: variantId }),
-              ...(versionId && { version_id: versionId }),
-              action_id: action,
-              category_id: privilege.category_id,
-              device_type: type,
-            },
-            { transaction },
-          );
-        }
-      }
-    });
-    if (!validActions.length) {
-      return {
-        success: false,
-        message: errorResponses.NO_VALID_ACTIONS_TO_UPDATE,
-        errorCode: statusCodes.STATUS_CODE_INVALID_FORMAT,
-        data: null,
-      };
-    }
-
-    if (isError.length) {
-      await transaction.rollback();
-      return {
-        success: false,
-        message: errorResponses.IMPROPER_CATEGORY_OR_ACTIONS,
-        errorCode: statusCodes.STATUS_CODE_DATA_NOT_FOUND,
-        data: null,
-      };
-    }
-    await transaction.commit();
-    return {
-      success: true,
-      message: successResponses.DEVICES_EDITED_SUCCESSFULLY,
     };
   } catch (err) {
     await transaction.rollback();
