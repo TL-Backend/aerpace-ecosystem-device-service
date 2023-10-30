caca9555b6cf7419067a2dc816ac6e814b027689
Satyanarayana Raju <133110337+satyanarayanaRaju21@users.noreply.github.com>
2023-10-30 11:06:07 +0530

    added env config

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 require('./sample.route')(router);
 require('./device.route')(router);
 require('./privilege.route')(router);
-require('./device.route')(router);
 
 module.exports = {
   router,
